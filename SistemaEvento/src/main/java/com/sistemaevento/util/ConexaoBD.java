f88d939388bacdb9ba74364d10e6fb093bfecfbc
ferreirayssa <xneesan07@gmail.com>
2025-06-11 13:37:26 -0300

    Segurança do banco

--- SistemaEvento/src/main/java/com/sistemaevento/util/ConexaoBD.java
+++ SistemaEvento/src/main/java/com/sistemaevento/util/ConexaoBD.java
@@ -3,21 +3,22 @@ package com.sistemaevento.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConexaoBD {
-    /*
-    private static final String HOST = "localhost";
-    private static final String PORT = "5439";
-    private static final String DATABASE = "postgres";
-    */
-    private static final String USUARIO = "postgres";
-    private static final String SENHA = "123";
+    private static final Dotenv dotenv = Dotenv.load();
 
-    private static final String URL = "jdbc:postgresql://localhost:5439/postgres";
+    private static final String HOST = dotenv.get("DB_HOST");
+    private static final String PORT = dotenv.get("DB_PORT");
+    private static final String DATABASE = dotenv.get("DB_NAME");
+    private static final String USUARIO = dotenv.get("DB_USER");
+    private static final String SENHA = dotenv.get("DB_PASS");
+
+    private static final String URL = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DATABASE;
 
     private Connection conn = null;
 
-   public Connection connect() {
+    public Connection connect() {
         try {
             Class.forName("org.postgresql.Driver");
             this.conn = DriverManager.getConnection(URL, USUARIO, SENHA);
